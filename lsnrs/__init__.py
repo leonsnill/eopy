b0b7ce4225e1ad5d384cda580d05a442fe595324
leonsnill <leon.nill@hu-berlin.de>
2019-10-30 14:23:55 +0100

    added Raster functions

---
+++ lsnrs/__init__.py
@@ -0,0 +1,8 @@
+# Handling
+from .Hand import list_files
+
+# Raster
+from .Raster import array_to_geotiff
+from .Raster import clip_raster
+from .Raster import mlr_array
+from .Raster import reproject_raster
