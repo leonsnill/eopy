39bf87708845e00b362829942b01f83f2a9dcc59
leonsnill <leon.nill@hu-berlin.de>
2019-11-17 11:56:18 +0100

    v0.3

--- lsnrs/Raster/__init__.py
+++ lsnrs/Raster/__init__.py
@@ -1,5 +1,4 @@
 from .array_to_gtiff import array_to_geotiff
-from .mlr_array import mlr_array
 from .reproject_raster import reproject_raster
 from .extent import extent
 from .img_offsets import img_offsets
