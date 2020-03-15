58cd8d677804aee7a5a2300e322f62d081efa1b4
leonnill <leon.nill@hu-berlin.de>
2020-03-15 16:41:45 +0100

    new

--- eopy/__init__.py
+++ eopy/__init__.py
@@ -1,2 +1,3 @@
 from eopy import vector, raster
 from .finder import finder
+from .random_sample import random_sample
--- eopy/vector/__init__.py
+++ eopy/vector/__init__.py
@@ -1,2 +1,4 @@
 from .coords_to_points import coords_to_points
 from .sample_random import sample_random
+from .single_to_multipolygon import single_to_multi
+from .rasterize_geom import rasterize_geom
